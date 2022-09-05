306555746e5544255c6df5ca512647dabbd12d80
maiano <98988636+maiano@users.noreply.github.com>
2022-09-05 23:55:14 +0200

    Add files via upload
    add kata

--- js/cw_js.js
+++ js/cw_js.js
@@ -43,4 +43,73 @@ function disemvowel(str) {
 
 // disemvowel = str => str.replace(/[aeiou]/gi,'');
 
+// Isograms
+
+function isIsogram(str){
+  str = str.toLowerCase();
+  return str.split('').every((el,i) => str.indexOf(el) === i);
+}
+
+// function isIsogram(str){
+//   return !str.match(/([a-z]).*\1/i);
+// }
+
+// Digits explosion
+
+function explode(s) {
+  return [...s].map(el => el.repeat(el)).join('');
+}
+
+// const explode = s => s.replace(/\d/g, d => d.repeat(d));
+
+// Human readable duration format
+
+function formatDuration (seconds) {
+  if (seconds === 0) return 'now';
+  const time = { year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1 };
+  let str = [];
+  for (var key in time) {
+    if (time[key] <= seconds) {
+      let temp = ~~(seconds/time[key]);
+      str.push(temp += temp > 1 ? ' ' + key + 's' : ' ' + key);
+      seconds = seconds % time[key];
+    }
+  }
+  return str.length > 1 ? str.join(', ').replace(/,([^,]*)$/,' and'+'$1') : str[0];
+}
+
+// Duplicate Encoder
+
+const duplicateEncode = word =>
+    word.toLowerCase()
+    .split('')
+    .map ((el, _, array) => array.filter(e => e === el).length > 1 ? ')' : '(')
+    .join('');
+
+// function duplicateEncode(word) {
+//   word = word.toLowerCase();
+//   return word.replace(/./g, m => word.indexOf(m) == word.lastIndexOf(m) ? '(' : ')');
+// }
+
+// Multiples of 3 or 5
+
+function solution(number){
+     if (number < 0) {
+          return 0;
+     } else {
+         return [...Array(number).keys()]
+         .filter(it => it % 3 == 0 || it % 5 == 0)
+         .reduce((prev, curr) => prev + curr, 0);
+     }
+}
+
+// N-th Fibonacci
+
+function nthFibo(n) {
+  let [prev, curr] = [0, 1];
+  for (let i = 1; i < n; i++) {
+    [prev, curr] = [curr, prev + curr];
+    }
+  return prev;
+}
 
