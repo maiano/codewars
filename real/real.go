bb3952aea76c52ada9e7fca93e5f742b6a70727e
maiano <98988636+maiano@users.noreply.github.com>
2022-12-19 21:17:35 +0100

    feat: add kata

--- real/real.go
+++ real/real.go
@@ -7,6 +7,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 // Two Sum
@@ -110,7 +111,6 @@ func GetSum(a, b int) int {
 func TwoToOne(s1 string, s2 string) string {
 	s := strings.Split((s1 + s2), "")
 	sort.Strings(s)
-	// var i int
 	i, prev := 0, ""
 	for _, v := range s {
 		if v != prev {
@@ -199,16 +199,6 @@ func duplicate_count(s1 string) int {
 	return count
 }
 
-// function duplicateEncode(word){
-// 	return word
-// 	  .toLowerCase()
-// 	  .split('')
-// 	  .map( function (a, i, w) {
-// 		return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
-// 	  })
-// 	  .join('');
-//   }
-
 func DuplicateEncode(word string) string {
 	w := []byte(strings.ToLower(word))
 	var b bytes.Buffer
@@ -237,16 +227,6 @@ func Order(s string) string {
 }
 
 // Tribonacci Sequence
-
-// func Tribonacci(signature [3]float64, n int) []float64 {
-// 	t := make([]float64, 0, n)
-// 	var a, b, c float64 = signature[0], signature[1], signature[2]
-// 	for i := 0; i < n; i++ {
-// 		t = append(t, a)
-// 		a, b, c = b, c, a+b+c
-// 	}
-// 	return t
-// }
 
 func Tribonacci(signature [3]float64, n int) []float64 {
 	t := signature[:]
@@ -278,6 +258,288 @@ func FindUniq(arr []float32) float32 {
 		}
 	}
 	return 0
+}
+
+// Split Strings
+
+func Solution(str string) []string {
+	return regexp.MustCompile(`.{2}`).FindAllString(str+"_", -1)
+}
+
+// Find the missing letter
+
+func FindMissingLetter(chars []rune) rune {
+	for i := 0; i < len(chars); i++ {
+		if next := chars[i] + 1; chars[i+1] != next {
+			return next
+		}
+	}
+	return 'a'
+}
+
+// Highest Scoring Word
+
+func High(s string) string {
+	c := strings.Fields(s)
+	max := score(c[0])
+	indexMax := 0
+	for i := 1; i < len(c); i++ {
+		if next := score(c[i]); max < next {
+			max = next
+			indexMax = i
+		}
+	}
+	return c[indexMax]
+}
+
+func score(s string) int {
+	var sum int
+	for _, v := range s {
+		sum += int(v) - (int('a') - 1)
+	}
+	return sum
+}
+
+func wave(s string) []string {
+	w := make([]string, 0, len(s))
+	for i := 0; i < len(s); i++ {
+		if unicode.IsLetter(rune(s[i])) {
+			w = append(w, (s[:i])+strings.ToUpper(string(s[i]))+s[i+1:])
+		}
+	}
+	return w
+}
+
+// Roman Numerals Decoder
+
+func Decode(roman string) int {
+
+	romanD := map[rune]int{'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000}
+
+	var sum, prev int
+
+	for _, v := range roman {
+		curr := romanD[v]
+		if curr > prev {
+			sum += curr - 2*prev
+		} else {
+			sum += curr
+		}
+		prev = curr
+	}
+	return sum
+}
+
+// Moving Zeros To The End
+
+func MoveZeros(arr []int) []int {
+	var j int
+	temp := make([]int, len(arr))
+	for i := 0; i < len(arr); i++ {
+		if arr[i] != 0 {
+			temp[j] = arr[i]
+			j++
+		}
+	}
+	return temp
+}
+
+// Valid Parentheses
+
+func ValidParentheses(parens string) bool {
+	var count int
+	for i := 0; i < len(parens); i++ {
+		switch parens[i] {
+		case '(':
+			count++
+		case ')':
+			count--
+		}
+		if count < 0 {
+			return false
+		}
+	}
+	return count == 0
+}
+
+// Human Readable Time
+
+func HumanReadableTime(sec int) string {
+	hh := sec / 3600
+	mm := sec % 3600 / 60
+	ss := sec % 60
+	return fmt.Sprintf("%.2d:%.2d:%.2d", hh, mm, ss)
+}
+
+// Directions Reduction
+
+func DirReduc(arr []string) []string {
+	op := map[string]string{"NORTH": "SOUTH", "EAST": "WEST", "WEST": "EAST", "SOUTH": "NORTH"}
+	path := make([]string, 0)
+
+	for _, v := range arr {
+		if len(path) > 0 && path[len(path)-1] == op[v] {
+			path = path[:len(path)-1]
+		} else {
+			path = append(path, v)
+		}
+	}
+	return path
+}
+
+// First non-repeating character
+
+func FirstNonRepeating(str string) string {
+
+	s := make(map[rune]int, 0)
+	for _, v := range strings.ToLower(str) {
+		s[v]++
+	}
+	for i, v := range strings.ToLower(str) {
+		if s[v] == 1 {
+			return string(str[i])
+		}
+	}
+	return ""
+}
+
+// Best travel
+
+func ChooseBestSum(t, k int, ls []int) int {
+	max := func(x, y int) int {
+		if x > y {
+			return x
+		}
+		return y
+	}
+	if t < 0 || k > len(ls) {
+		return -1
+	}
+	if k == 0 {
+		return 0
+	}
+	c0 := ChooseBestSum(t, k, ls[1:])
+	c1 := ChooseBestSum(t-ls[0], k-1, ls[1:])
+	if c1 < 0 {
+		return c0
+	}
+	return max(c0, ls[0]+c1)
+}
+
+// int32 to IPv4
+
+func Int32ToIp(n uint32) string {
+	ip := make([]byte, 0)
+	for i := 0; i < 4; i++ {
+		ip = append(ip, byte(n))
+		n = n >> 8
+	}
+	return fmt.Sprintf("%d.%d.%d.%d", ip[3], ip[2], ip[1], ip[0])
+}
+
+// Josephus Permutation
+
+func Josephus(items []interface{}, k int) []interface{} {
+	res := []interface{}{}
+	var i int
+	for len(items) > 0 {
+		i = (i + k - 1) % len(items)
+		res = append(res, items[i])
+		items = append(items[:i], items[i+1:]...)
+	}
+	return res
+}
+
+// Not very secure
+
+func alphanumeric(str string) bool {
+	var valid = regexp.MustCompile(`^\w+$`)
+	return valid.MatchString(str)
+}
+
+// MiniStringFuck
+
+func Interpreter(code string) string {
+	var buf bytes.Buffer
+	var count byte
+
+	for _, v := range code {
+		if v == '+' {
+			count++
+		}
+		if v == '.' {
+			buf.WriteByte(count)
+		}
+	}
+	return buf.String()
+}
+
+// Beeramid
+
+func Beeramid(bonus int, price float64) int {
+	var count int
+	maxCan := int(float64(bonus) / price)
+	for (count+1)*(count+1) <= maxCan {
+		count++
+		maxCan -= count * count
+	}
+	return count
+}
+
+// ISBN-10 Validation
+
+func ValidISBN10(isbn string) bool {
+	isbn = strings.ToUpper(isbn)
+	if ok, _ := regexp.MatchString(`[0-9]{9}[X0-9]{1}`, isbn); !ok {
+		return false
+	}
+	var result int
+	for i := 0; i < 9; i++ {
+		result += (i + 1) * (int(isbn[i]) - 48)
+	}
+	if isbn[9] == 'X' {
+		result += 100
+	} else {
+		result += 10 * (int(isbn[9]) - 48)
+	}
+	return result%11 == 0
+}
+
+// ROT13
+
+func Rot13(msg string) string {
+	var buf bytes.Buffer
+	for _, v := range msg {
+		b := byte(v)
+		if b >= 'A' && b <= 'Z' {
+			b = 'A' + (b-'A'+13)%26
+		} else if b >= 'a' && b <= 'z' {
+			b = 'a' + (b-'a'+13)%26
+		}
+		buf.WriteByte(b)
+	}
+	return buf.String()
+}
+
+// WeIrD StRiNg CaSe
+
+func toWeirdCase(str string) string {
+	str = strings.ToLower(str)
+	var result []string
+	for _, v := range strings.Fields(str) {
+		s := []rune(v)
+		for i := 0; i < len(s); i += 2 {
+			s[i] = unicode.ToUpper(s[i])
+		}
+		result = append(result, string(s))
+	}
+	return strings.Join(result, " ")
 }
 
 func main() {
